aca2634287837f017cdb5e5f00ebc790a68c3815
Arjun <arjunsah856@gmail.com>
2025-09-27 22:02:49 +0545

    adder navbar and card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <div>
-      
+      <>
+        <Navbar />
+        <Card />
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
