278581d85aa2ee6915b2aadea5ccd9fcf7887e93
Arjun <arjunsah856@gmail.com>
2025-09-29 13:56:49 +0545

    second approach to handle submission of form data

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -9,33 +9,57 @@ const CreateBlog = () => {
   const navigate = useNavigate()
 
   //1st approach to handle submission of form data
-  const [title, setTitle] = useState("")
-  const [subTitle, setSubTitle] = useState("")
-  const [description, setDescription] = useState("")
+  // const [title, setTitle] = useState("")
+  // const [subTitle, setSubTitle] = useState("")
+  // const [description, setDescription] = useState("")
   
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()  //avoids the refresh of page when form submitted 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()  //avoids the refresh of page when form submitted 
 
-    //changing the form data(array) to object  ---> because post method only accepts (url, object)
-    const data = {
-      title: title,
-      subTitle: subTitle,
-      description: description
-    }
+  //   //changing the form data(array) to object  ---> because post method only accepts (url, object)
+  //   const data = {
+  //     title: title,
+  //     subTitle: subTitle,
+  //     description: description
+  //   }
 
-    console.log(data)
+  //   console.log(data)
     
-    const res = await axios.post('http://localhost:2000/createBlog', data)
+  //   const res = await axios.post('http://localhost:2000/createBlog', data)
 
-    if(res.status === 201){
-      alert(res.data.message)
-      navigate('/')
-    } else {
-      alert('Something went wrong')
-    }
+  //   if(res.status === 201){
+  //     alert(res.data.message)
+  //     navigate('/')
+  //   } else {
+  //     alert('Something went wrong')
+  //   }
+  // }
+
+
+  //2nd approach to handle form data submission
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const formData = new FormData(e.currentTarget)   // mskes object as {key: value}
+  console.log("formdata", formData)  // formData is in array
+
+  const data = Object.fromEntries(formData)  //changed in object
+
+  console.log("data", data)
+
+  //sending to backend
+  const res = await axios.post("http://localhost:2000/createBlog", data)
+
+  if(res.status === 201) {
+    alert(res.data.message)
+    navigate('/')
+  } 
+  else {
+    alert("Something went wrong")
   }
 
+}
 
 
 
@@ -48,15 +72,15 @@ const CreateBlog = () => {
     <form className="form" onSubmit={handleSubmit}>
       {/* Title */}
       <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" placeholder="Enter blog title" onChange={(e) => setTitle(e.target.value)} />
+      <input type="text" id="title" name="title" placeholder="Enter blog title" />
 
       {/* Subtitle */}
       <label htmlFor="subtitle">Subtitle</label>
-      <input type="text" id="subtitle" name="subTitle" placeholder="Enter blog subtitle" onChange={(e) => setSubTitle(e.target.value)} />
+      <input type="text" id="subtitle" name="subTitle" placeholder="Enter blog subtitle" />
 
       {/* Description */}
       <label htmlFor="description">Description</label>
-      <textarea id="description" name="description" rows="4" placeholder="Write your blog description..." onChange={(e) => setDescription(e.target.value)} />
+      <textarea id="description" name="description" rows="4" placeholder="Write your blog description..." />
 
       {/* Submit Button */}
       <button type="submit" className="btn-submit">Publish Blog</button>
